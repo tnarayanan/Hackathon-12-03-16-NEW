a5f09982620e96a97984060d135de92f5e14a261
tnarayanan <tejasn100@gmail.com>
2016-12-03 13:10:52 -0800

    Added send and receive activity

---
+++ app/src/main/java/com/example/tejas/hackathon_12_03_16/RecieveActivity.java
@@ -0,0 +1,13 @@
+package com.example.tejas.hackathon_12_03_16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecieveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieve);
+    }
+}
