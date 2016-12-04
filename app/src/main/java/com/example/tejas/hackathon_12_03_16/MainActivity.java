795f7e720bdde1c482b68dc70410fa871a3b1fec
RazrSlyr <sharpedo75@gmail.com>
2016-12-03 18:22:18 -0800

    Stuff

--- app/src/main/java/com/example/tejas/hackathon_12_03_16/MainActivity.java
+++ app/src/main/java/com/example/tejas/hackathon_12_03_16/MainActivity.java
@@ -1,72 +1,41 @@
 package com.example.tejas.hackathon_12_03_16;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import static com.example.tejas.hackathon_12_03_16.R.id.time;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText name;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        button = (Button) findViewById(R.id.Go);
+        name = (EditText) findViewById(R.id.name2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.child("Users").push().setValue(name.getText().toString());
+
+                SendActivity.acceptedNames.add(name.getText().toString());
+                startActivity(new Intent(getApplicationContext(), SendActivity.class));
+            }
+        });
     }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
-
-
 }
--- app/src/main/java/com/example/tejas/hackathon_12_03_16/SendActivity.java
+++ app/src/main/java/com/example/tejas/hackathon_12_03_16/SendActivity.java
@@ -5,42 +5,75 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class SendActivity extends AppCompatActivity {
+    EditText name;
+    String blank;
     TimePicker alarm;
     Button send;
     CharSequence timeSet;
+    public static ArrayList acceptedNames = new ArrayList<String>();
+    Boolean nameAccepted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+
+        acceptedNames.add("Sripad");
+        acceptedNames.add("Tejas");
         alarm = (TimePicker) findViewById(R.id.alarmSend);
         alarm.setIs24HourView(false);
         send = (Button) findViewById(R.id.send);
+        name = (EditText) findViewById(R.id.toWho);
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(alarm.getHour() >= 12){
-                    timeSet = "pm";
+                if (name.getText().toString().equals("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "NO NOMBRE AQUI", Toast.LENGTH_SHORT);
+                    toast.show();
                 } else {
-                    timeSet = "am";
+                    for(int i = 0; i < acceptedNames.size(); i++){
+                        if(nameAccepted == false) {
+                            nameAccepted = name.getText().toString().equals(acceptedNames.get(i).toString());
+                        }
+                    }
+                    if(nameAccepted) {
+                        if (alarm.getHour() >= 12) {
+                            timeSet = "pm";
+                        } else {
+                            timeSet = "am";
+                        }
+                        Integer hour = alarm.getHour();
+                        if (hour == 0) {
+                            hour = 12;
+                        } else if (hour > 12) {
+                            hour -= 12;
+                        }
+                        Integer minute = alarm.getMinute();
+                        String strMinute = minute.toString();
+                        if (minute < 10) {
+                            strMinute = "0" + strMinute;
+                        }
+                        //Toast toast = Toast.makeText(getApplicationContext(), name.getText().equals("") + "", Toast.LENGTH_SHORT);
+                        Toast toast = Toast.makeText(getApplicationContext(), hour + ":" + strMinute + timeSet + " to " + name.getText(), Toast.LENGTH_SHORT);
+                        toast.show();
+                        nameAccepted = false;
+                        Intent thing = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(thing);
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), name.getText().toString() + " isn't a registered user", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
                 }
-                Integer hour = alarm.getHour();
-                if(hour == 0){
-                    hour = 12;
-                } else if(hour > 12){
-                    hour -= 12;
-                }
-                Integer minute = alarm.getMinute();
-                String strMinute = minute.toString();
-                if(minute < 10){
-                    strMinute = "0" + strMinute;
-                }
-                Toast toast = Toast.makeText(getApplicationContext(), hour + ":" + strMinute + timeSet, Toast.LENGTH_SHORT);
-                toast.show();
             }
         });
 
